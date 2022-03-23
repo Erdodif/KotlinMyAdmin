0bda8caefbcb2fa174e840687e74ae1b7248edcc
Erdodif <erdodif@gmail.com>
2022-03-23 20:50:25 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    application
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+kotlin {
+    jvm("KotlinBackend") {
+        compilations.all {
+            kotlinOptions.jvmTarget = "13"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+        withJava()
+    }
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val KotlinBackendMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-netty:1.6.3")
+                implementation("io.ktor:ktor-html-builder:1.6.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+            }
+        }
+        val KotlinBackendTest by getting
+        val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.240-kotlin-1.5.30")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.240-kotlin-1.5.30")
+            }
+        }
+        val jsTest by getting
+    }
+}
+
+application {
+    mainClass.set("ServerKt")
+}
+
+tasks.named<Copy>("KotlinBackendProcessResources") {
+    val jsBrowserDistribution = tasks.named("jsBrowserDistribution")
+    from(jsBrowserDistribution)
+}
+
+tasks.named<JavaExec>("run") {
+    dependsOn(tasks.named<Jar>("KotlinBackendJar"))
+    classpath(tasks.named<Jar>("KotlinBackendJar"))
+}
